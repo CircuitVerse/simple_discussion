313b51da81776d732556e14e4feb3a7e055c6d79
Aboobacker MK <aboobackervyd@gmail.com>
2025-08-30 13:54:31 +0530

    fix: rails 8 compatiability

--- app/models/spam_report.rb
+++ app/models/spam_report.rb
@@ -5,7 +5,7 @@ class SpamReport < ApplicationRecord
   validates :forum_post_id, :user_id, :reason, presence: true
   validates :details, presence: true, if: -> { reason == "others" }
 
-  enum reason: {
+  enum :reason, {
     sexual_content: 0,
     violent_content: 1,
     irrelevant_content: 2,
